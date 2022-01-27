1a67bff30629e4b424da7cbc91113822ff6abb1f
Takahiro <tkhr_621@docomo.ne.jp>
2022-01-27 21:55:57 +0900

    ディレクトリ構成変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: true
+  },
+  build: {
+    // root (= ./src) から見た相対パスで指定
+    outDir: '../public',
+  },
   plugins: [react()]
 })
